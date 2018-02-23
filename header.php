4e68705e50b24ff4aac4c88a0b81216b783464cc
esmi43 <thisismecestmoi@gmail.com>
2018-02-23 15:56:56 +0100

    header

--- header.php
+++ header.php
@@ -7,6 +7,34 @@
 			<div class="color" id="Bleu" title="Change la couleur du site" data-couleur="#005DA3"></div>
 			<div class="color" id="Rose" title="Change la couleur du site" data-couleur="#FF456A"></div>
 		</div>
-            <div id="lore"><a>Login</a>
+          
+			<div id="lore"><a>
+				
+				<button id = "button-modal" onclick = "openmodal()" > Login </button>
+				</a>
+			 <div id = "modal">
+	
+				<form id="formulaire" method="post" action="formulaire.php">
+					 
+				<h1> Connexion </h1> 
+			         
+				<h2> adresse mail : </h2>
+				</br>
+				<input type="text" name="mail"/>
+				</br>
+				</br>
+				<h2> mot de passe : </h2>
+				</br>
+				<input type="password" name="mdp"/>
+				</br>
+				</br>
+				<p>
+				<input type="submit" value="valider" /> 
+				</p>
+				</form>
+				<button id="close" onclick="CloseModal()"> X </button>
+			</div>  
             <a> Register</a></div>
     </header>
+	
+<script type="text/javascript" src="affichage.js"> </script>
